8a18132706501c59ef44aba9a8f24aec9d978712
https://github.com/DavidUEA/Tareagym.git <dh.changoa@uea.edu.ec>
2024-06-17 21:36:59 -0500

    Merge remote-tracking branch 'origin/master'

--- Pg_objetos_temperatura.py
+++ Pg_objetos_temperatura.py
@@ -1,3 +1,13 @@
 class Temperatura:
-    dia=""
-    def __init__(self)
+    def __init__(self,n1,n2,n3,n4,n5,n6,n7):
+        self.lunes= n1
+        self.martes= n2
+        self.miercoles= n3
+        self.jueves= n4
+        self.viernes= n5
+        self.sabado= n6
+        self.domingo= n7
+        self.total= ((n1 + n2 + n3 + n4 + n5 + n6 + n7)/7)
+cal_temperaturas = Temperatura (22,23,24,25,26,27, 28)
+print(cal_temperaturas.total)
+
